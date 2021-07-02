1d710e1886834068b99eb62b2dfaf79133ae81fc
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2021-07-02 09:42:16 +0100

    tidy up

--- app_with_workflow/views.py
+++ app_with_workflow/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render, reverse, redirect
-from django.utils import timezone
 from django.views import View
-from django.views.generic.edit import FormView, CreateView
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.http import JsonResponse
 
-from app_with_workflow.forms import ApproveRequirementForm
 from workflow.executor import WorkflowExecutor
-
 from workflow.models import Flow
 
 
@@ -66,38 +62,3 @@ class FlowContinueView(View):
         )
 
         return redirect(reverse("flow", args=[flow.pk]))
-
-
-# Starts process
-class RequirementView(FormView):
-    template_name = "requirement.html"
-    form_class = ApproveRequirementForm
-
-    def get(self, request, *args, **kwargs):
-        form = self.form_class(instance=self.requirement)
-
-        return render(
-            request,
-            self.template_name,
-            {
-                "form": form,
-            },
-        )
-
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(
-            request.POST,
-            instance=self.requirement,
-        )
-
-        if form.is_valid():
-            form.save()
-            process = WorkflowExecutor.start_process()
-
-        return render(
-            request,
-            self.template_name,
-            {
-                "form": form,
-            },
-        )
